fcbe9249a5ec1788b60ef981902bfe0e1b2f6a7d
Corbin Hall <hallcorbin92@gmail.com>
2020-08-09 12:58:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var theBeatlesPlay = '';
 function theBeatlesPlay(musician, instrument) {
   for(let i = 0; i < 4; i++) {
-    return `${theBeatlesPlay.push(`${musician[i]} plays ${instrument[i]}.`)}`
+    theBeatlesPlay.push(`${musician[i]} plays ${instrument[i]}.`)
+    return theBeatlesPlay
   }
 }
 
