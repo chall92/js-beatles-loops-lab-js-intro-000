9da0272119699ef53ebfe2e96ae56568b835e3a5
Corbin Hall <hallcorbin92@gmail.com>
2020-08-09 14:34:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ function johnLennonFacts(fact) {
 var johnLennonFacts = [];
 var i = 0;
 while(johnLennonFacts.length < fact.length) {
-  johnLennonFacts.push(fact[i] + '!!!');
-  i++;
+    johnLennonFacts.push(fact[i] + '!!!');
+    i++;
 } return johnLennonFacts
 
 }
 
 function iLoveTheBeatles(num) {
-
+do() {
+  while()
+}
 }
